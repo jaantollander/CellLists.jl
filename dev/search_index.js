9b6c1e0ee74943daac4f53e25f0c0f7ed92601de
Documenter.jl <documenter@juliadocs.github.io>
2020-09-25 11:09:55 +0000

    build based on 296917c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CellLists.jl","page":"CellLists.jl","title":"CellLists.jl","text":"","category":"section"},{"location":"","page":"CellLists.jl","title":"CellLists.jl","text":"Documentation for CellLists.jl","category":"page"}]
+}
