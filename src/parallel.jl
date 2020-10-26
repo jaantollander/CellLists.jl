d3f4e26600a64f2de1ab2d773550d9637e134fa0
Jaan Tollander de Balsch <dev@jaantollander.com>
2020-10-26 10:47:41 +0200

    Constructor simplification

--- src/parallel.jl
+++ src/parallel.jl
@@ -10,9 +10,9 @@ function CellList(p::Array{T, 2}, r::T, ::Val{:parallel}) where T <: AbstractFlo
     t = nthreads()
     n, d = size(p)
     cs = cumsum(fill(fld(n, t), t-1))
-    parts = collect(enumerate(zip([0; cs], [cs; n])))
+    parts = zip([0; cs], [cs; n])
     res = Array{CellList{d}, 1}(undef, t)
-    @threads for (i, (a, b)) in parts
+    @threads for (i, (a, b)) in collect(enumerate(parts))
         res[i] = CellList(p[(a+1):b, :], r, a)
     end
     return reduce(merge, res)
